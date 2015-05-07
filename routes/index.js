7bf4719e775bd93b97885340f993ef974e96c6d5
belen <merodio13@gmail.com>
2015-05-07 11:45:32 +0200

    Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,10 @@ router.param('quizId', quizController.load);  //autoload : quizId
 
 //definicion de rutas en quizes
 
-router.get('/quizes',         quizController.index);
-router.get('/quizes/:quizId(\\d+)' , quizController.show);
+router.get('/quizes',                       quizController.index);
+router.get('/quizes/:quizId(\\d+)' ,        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer' , quizController.answer);
+router.get('/quizes/new' ,                  quizController.new);
+router.post('/quizes/create' ,              quizController.create);
 
 module.exports = router;
