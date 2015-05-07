4b962efcc2eb68a8e50f19292fbbf002ba0110d0
belen <merodio13@gmail.com>
2015-05-07 10:46:31 +0200

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,15 @@
 var models = require('../models/models.js');
-// GET /quizes/:id
-exports.show = function(req , res){
-models.Quiz.find(req.params.quizId).then(function(quiz){
-res.render('quizes/show' , {quiz: quiz});
-})
+// Autoload - factoriza el codigo si la ruta incluye: quizId
+exports.load = function(req , res, next, quizId){
+models.Quiz.find(quizId).then(
+function(quiz) {
+if (quiz) {
+req.quiz = quiz;
+next();
+}else {next(new Error ('No existe quizId=' + quizId)); }
+}
+).catch(function(error){ next(error);});
+
 };
 
 //GET / quizes
@@ -11,6 +17,12 @@ exports.index = function(req , res){
 models.Quiz.findAll().then(function(quizes){
 res.render('quizes/index.ejs' , {quizes: quizes});
 })
+};
+// GET /quizes/:id
+exports.show = function(req , res){
+
+res.render('quizes/show' , {quiz: req.quiz});
+
 };
 
 
@@ -23,16 +35,14 @@ res.render('quizes/index.ejs' , {quizes: quizes});
 
 exports.answer = function (req , res){
 
-models.Quiz.find(req.params.quizId).then(function(quiz){{
+var resultado = 'Incorrecto';
 
-if (req.query.respuesta === quiz.respuesta){
-res.render('quizes/answer' ,
+if (req.query.respuesta === req.quiz.respuesta){
+
 
  
-{quiz: quiz, respuesta: 'Correcto'});
+resultado = 'Correcto';
 }
-else {res.render('quizes/answer' , {quiz: quiz, respuesta: 'incorrecto'});
+{res.render('quizes/answer' , {quiz: req.quiz, respuesta: resultado});
 
-}
-})
 };
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+//autoload de comandos con :quizId
+router.param('quizId, quizController.load);  //autoload : quizId
+
+//definicion de rutas en quizes
+
 router.get('/quizes',         quizController.index);
 router.get('/quizes/:quizId(\\d+)' , quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer' , quizController.answer);
