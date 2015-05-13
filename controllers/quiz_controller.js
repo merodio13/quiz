842bc1a60876933a6a556a2f4815e24f466c5d4a
belen <merodio13@gmail.com>
2015-05-13 18:54:22 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -47,6 +47,11 @@ exports.show = function (req , res){
 res.render('quizes/show' , {quiz : req.quiz ,errors: []});
 };
 
+//GET /quizes/:id/edit
+exports.edit = function (req , res){
+var quiz = req.quiz;
+res.render('quizes/edit' , {quiz : quiz ,errors: []});
+};
 
 // GET /quizes/new
 
@@ -76,6 +81,28 @@ quiz
 }
 }
  );
+};
+
+
+// PUT /quizes/:id
+
+exports.update= function (req, res) {
+     req.quiz.pregunta = req.body.quiz.preguntas;
+     req.quiz.respuesta = req.body.quiz.respuesta;
+
+req.quiz
+.validate()
+.then(
+function (err){
+if(err){
+res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+ }else{
+req.quiz
+.save({fields: ["pregunta" , "respuesta"]})
+.then( function(){res.redirect('/quizes');});
+}
+}
+);
 };
 
 
