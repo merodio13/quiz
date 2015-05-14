1d1a87acbe723db7281040d0784118414c2d1121
belen <merodio13@gmail.com>
2015-05-14 18:08:11 +0200

    idem

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,7 @@
 var users = { admin: {id:1, username:"admin" , password:"1234"},
-              pepe: {id:2, username:"pepe" , password:"5678"}
+              pepe: {id:2, username:"pepe" , password:"5678"},
+              clara: {id:3, username:"clara" , password:"clara"},
+              belen: {id:4, username:"belen" , password:"belen"}
              };
 
 //Comprueba si el usuario esta regustrado en users
