20e692bd8bc02b3126f4123161c716d34e481bd0
belen <merodio13@gmail.com>
2015-05-14 17:23:00 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,12 @@ var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' , errors: []});
+res.render('index', { title: 'Quiz' , errors: []});
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);
+router.param('quizId', quizController.load);// autoload: quizId
+router.param('commentId', commentController.load); // autoload: coomentId
 
 // definicion de rutas de sesion
 router.get('/login', sessionController.new); //formulario loggin
@@ -31,4 +32,7 @@ router.delete('/quizes/:quizId(\\d+)',sessionController.loginRequired,  quizCont
 //Definicion de rutas con comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+sessionController.loginRequired,  commentController.publish);
+
 module.exports = router;
